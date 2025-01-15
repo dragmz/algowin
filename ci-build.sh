1fe24bee0c51a9e75f50df623eba1a8f9dc99b12
Marcin Zawiejski <dragmz@gmail.com>
2025-01-15 17:51:57 +0100

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -1,8 +1,8 @@
 pwd
 ls -la ~
 
-wget -q https://go.dev/dl/go1.20.6.windows-amd64.zip
-unzip -q go1.20.6.windows-amd64.zip
+wget -q https://go.dev/dl/go1.23.4.windows-amd64.zip
+unzip -q go1.23.4.windows-amd64.zip
 
 export PATH=$PATH:$(pwd)/go/bin
 export GOROOT=$(pwd)/go
